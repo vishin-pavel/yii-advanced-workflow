d5d623790e944911703e6358d9b83d66c4fbb543
vishin.pavel <it.vishin@gmail.com>
2017-10-05 00:15:38 +0300

    дополнения к конфигам

--- src/environments/dev/common/config/main-local.php
+++ src/environments/dev/common/config/main-local.php
@@ -3,14 +3,16 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=mariadb;dbname=app',
+            'username' => 'user',
+            'password' => '123',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'host' => 'mailhog',
+            'port' => 1025,
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
